7037733070ec53dce839a2ca0753d9ab30cffb98
Lars Kroll <lkroll@zi.de>
2020-05-06 16:39:30 +0200

    Updated Dashboard

---
+++ R/update_dashboard.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Read Env Variables"
+source ~/myenv.sh
+echo "Starting conda"
+source /opt/tljh/user/bin/conda
+conda activate "R_36"
+echo "Pulling covid-19 Code from git"
+cd ~/covid19dashboard
+git pull
+echo "Starting DB-Update"
+cd ~/covid-19/R/
+/home/jupyter-lekroll/.conda/envs/R_36/bin/R CMD BATCH renderdashboard.R
+git add *
+git commit -m "Updated Dashboard"
+git push
