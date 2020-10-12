ee622794a6023d9f445be2e142f8c6080aec2005
Edgar <edgar.steiger+github@gmail.com>
2020-10-12 18:45:22 +0200

    7-tage-inzidenz nach altersgruppen in tabellen

--- R/functions.R
+++ R/functions.R
@@ -79,13 +79,14 @@ rki_divi_n_alter <- rki %>% group_by(Meldedatum,Altersgruppe) %>%
          "itsfaelle"=`faelle_covid_aktuell`/`Infected`,
          'Todesfälle'= `Todesfälle gesamt`/ `Fälle gesamt`) 
 rki_alter_destatis <- rki %>% 
+  group_by(Meldedatum, Altersgruppe, IdLandkreis) %>% # this takes long unfortunately...
+  summarise(AnzahlFall=sum(AnzahlFall,na.rm = T),
+            AnzahlTodesfall=sum(AnzahlTodesfall,na.rm=T)) %>% 
+  # arrange(Meldedatum,Altersgruppe) %>%
+  collect() %>%
   filter(Altersgruppe!="unbekannt") %>%
   mutate(id=as.integer(IdLandkreis)*1000) %>%
   filter(!is.na(id)) %>%
-  group_by(Meldedatum, Altersgruppe, id) %>% 
-  summarise(AnzahlFall=sum(AnzahlFall,na.rm = T),
-            AnzahlTodesfall=sum(AnzahlTodesfall,na.rm=T)) %>% 
-  arrange(Meldedatum,Altersgruppe) %>% collect() %>%
   mutate(Altersgruppe=str_remove_all(Altersgruppe,"A"),
          Altersgruppe=ifelse(Altersgruppe %in% c("60-79","80+"),
                                       "60+",
@@ -94,12 +95,34 @@ rki_alter_destatis <- rki %>%
   group_by(Meldedatum,Altersgruppe, id) %>% 
   summarise("Fälle"=sum(AnzahlFall , na.rm = T),
             "Todesfälle"=sum(AnzahlTodesfall , na.rm=T)) %>% 
-  arrange(Meldedatum,Altersgruppe, id) %>% 
+  # arrange(Meldedatum,Altersgruppe, id) %>% 
   pivot_wider(id_cols = c(Meldedatum, id),
               names_from = Altersgruppe,
               values_from = c("Fälle","Todesfälle"),
               values_fill = list("Fälle"=0,"Todesfälle"=0)) %>% ungroup() %>%
-  mutate(Meldedatum=lubridate::as_date(Meldedatum))
+  mutate(Meldedatum=lubridate::as_date(Meldedatum), blid=floor(id/1000000))
+rki_alter_destatis <- bind_rows(rki_alter_destatis,
+                                 rki_alter_destatis %>%
+                                   group_by(Meldedatum, blid) %>%
+                                   summarise(`Fälle_15-34`=sum(`Fälle_15-34`),
+                                             `Fälle_35-59`=sum(`Fälle_35-59`),
+                                             `Fälle_0-15`=sum(`Fälle_0-15`),
+                                             `Fälle_60+`=sum(`Fälle_60+`),
+                                             `Todesfälle_15-34`=sum(`Todesfälle_15-34`),
+                                             `Todesfälle_35-59`=sum(`Todesfälle_35-59`),
+                                             `Todesfälle_0-15`=sum(`Todesfälle_0-15`),
+                                             `Todesfälle_60+`=sum(`Todesfälle_60+`)) %>%
+                                   mutate(id=blid),
+                                 rki_alter_destatis %>% group_by(Meldedatum) %>%
+                                  summarise(`Fälle_15-34`=sum(`Fälle_15-34`),
+                                            `Fälle_35-59`=sum(`Fälle_35-59`),
+                                            `Fälle_0-15`=sum(`Fälle_0-15`),
+                                            `Fälle_60+`=sum(`Fälle_60+`),
+                                            `Todesfälle_15-34`=sum(`Todesfälle_15-34`),
+                                            `Todesfälle_35-59`=sum(`Todesfälle_35-59`),
+                                            `Todesfälle_0-15`=sum(`Todesfälle_0-15`),
+                                            `Todesfälle_60+`=sum(`Todesfälle_60+`)) %>%
+                                   mutate(id=0))
 rki_alter_bund <- rki %>% 
   filter(Altersgruppe!="unbekannt") %>%
   mutate(id=as.integer(IdLandkreis)*1000) %>%
@@ -121,8 +144,8 @@ rki_alter_bund <- rki %>%
               values_from = c("Fälle","Todesfälle"),
               values_fill = list("Fälle"=0,"Todesfälle"=0)) %>% ungroup() %>%
   mutate(Meldedatum=lubridate::as_date(Meldedatum))
-strukturdaten <- tbl(conn,"strukturdaten")
-aktuell <- tbl(conn,"params")
+strukturdaten <- tbl(conn,"strukturdaten") %>% collect()
+aktuell <- tbl(conn,"params") %>% collect()
 trends <- tbl(conn,"trends")
 brd_testungen <- tbl(conn,"brd_testungen") %>% collect()
 Datenstand <- tbl(conn,"Stand") %>% collect()
@@ -241,24 +264,45 @@ letzte_7_tage_altersgruppen_bund <-  rki_alter_bund %>%
   # mutate(`Faelle_letzte_7_Tage_je100TsdEinw_0-59`=round(`Faelle_letzte_7_Tage_0-59`/(sum(select_(., "`unter 3 Jahre`:`55 bis unter 60 Jahre`"))/100000)),
   #        `Faelle_letzte_7_Tage_je100TsdEinw_60-79`=round(`Faelle_letzte_7_Tage_60-79`/(sum(select_(., "`unter 3 Jahre`:`55 bis unter 60 Jahre`"))/100000)),
   #        `Faelle_letzte_7_Tage_je100TsdEinw_80+`=round(`Faelle_letzte_7_Tage_80+`/(sum(select_(., "`unter 3 Jahre`:`55 bis unter 60 Jahre`"))/100000)))
+letzte_7_tage_altersgruppen_destatis <- rki_alter_destatis %>%
+  mutate(date=date(Meldedatum)) %>%
+  group_by(id) %>% arrange(id,-as.numeric(date)) %>%
+  filter(row_number()<=7) %>% # hier sind tatsächliche Fälle und nicht cumsum
+  summarise(`Faelle_letzte_7_Tage_0-14`=sum(`Fälle_0-15`),
+            `Faelle_letzte_7_Tage_15-34`=sum(`Fälle_15-34`),
+            `Faelle_letzte_7_Tage_35-59`=sum(`Fälle_35-59`),
+            `Faelle_letzte_7_Tage_60+`=sum(`Fälle_60+`)) %>%
+  ungroup() %>%
+  left_join(., strukturdaten, by="id") %>%
+  mutate(`Faelle_letzte_7_Tage_je100TsdEinw_0-14`=round(`Faelle_letzte_7_Tage_0-14`/((`unter 3 Jahre`+`3 bis unter 6 Jahre`+`6 bis unter 10 Jahre`+`10 bis unter 15 Jahre`)/100000)),
+         `Faelle_letzte_7_Tage_je100TsdEinw_15-34`=round(`Faelle_letzte_7_Tage_15-34`/((`15 bis unter 18 Jahre`+`18 bis unter 20 Jahre`+`20 bis unter 25 Jahre`+`25 bis unter 30 Jahre`+`30 bis unter 35 Jahre`)/100000)),
+         `Faelle_letzte_7_Tage_je100TsdEinw_35-59`=round(`Faelle_letzte_7_Tage_35-59`/((`35 bis unter 40 Jahre`+`40 bis unter 45 Jahre`+`45 bis unter 50 Jahre`+`50 bis unter 55 Jahre`+`55 bis unter 60 Jahre`)/100000)),
+         `Faelle_letzte_7_Tage_je100TsdEinw_60+`=round(`Faelle_letzte_7_Tage_60+`/((`60 bis unter 65 Jahre`+`65 bis unter 75 Jahre`+`75 Jahre und mehr`)/100000)))
+
 ausgangsdaten <- aktuell  %>%
-  select(id,name,ICU_Betten,Einwohner,ebene,
+  select(id,name,ICU_Betten,EW_insgesamt,ebene,
          cases,R0) %>% filter(ebene!="Staaten" & !is.na(ebene)) %>% select(-ebene) %>% collect() %>%
   left_join(.,letzte_7_tage,by="id") %>%
-  mutate(Faelle_letzte_7_Tage_je100TsdEinw=round(Faelle_letzte_7_Tage/(Einwohner/100000)),
+  left_join(., letzte_7_tage_altersgruppen_destatis %>% select(
+    id,
+    `Faelle_letzte_7_Tage_je100TsdEinw_0-14`,
+    `Faelle_letzte_7_Tage_je100TsdEinw_15-34`,
+    `Faelle_letzte_7_Tage_je100TsdEinw_35-59`,
+    `Faelle_letzte_7_Tage_je100TsdEinw_60+`), by="id") %>%
+  mutate(Faelle_letzte_7_Tage_je100TsdEinw=round(Faelle_letzte_7_Tage/(EW_insgesamt/100000)),
          Faelle_letzte_7_Tage_je100TsdEinw=ifelse(Faelle_letzte_7_Tage_je100TsdEinw<0,NA,Faelle_letzte_7_Tage_je100TsdEinw))
 
 
 # vorwarnzeit aktueller tag daten
 vorwarnzeitergebnis <- ausgangsdaten %>%
-  mutate(Handlungsgrenze_7_tage=50*(Einwohner/100000),
+  mutate(Handlungsgrenze_7_tage=50*(EW_insgesamt/100000),
          Handlungsgrenze_pro_Tag=round(Handlungsgrenze_7_tage/7),
          R0 = ifelse((R0>1) & (Faelle_letzte_7_Tage_pro_Tag==0),NA,R0),
          Kapazitaet=ICU_Betten*0.25/share_icu/icu_days,
          Auslastung_durch_Grenze=round(100*(Handlungsgrenze_pro_Tag/Kapazitaet)))
 
 myTage <- vorwarnzeitergebnis %>% rowwise() %>%
-  do(Tage = vorwarnzeit_berechnen(.$Einwohner, .$cases,.$Faelle_letzte_7_Tage_pro_Tag,.$Kapazitaet,1.3)) %>%
+  do(Tage = vorwarnzeit_berechnen(.$EW_insgesamt, .$cases,.$Faelle_letzte_7_Tage_pro_Tag,.$Kapazitaet,1.3)) %>%
   unnest(cols = c(Tage), keep_empty=TRUE)
 vorwarnzeitergebnis <- vorwarnzeitergebnis %>%
   mutate(Vorwarnzeit = myTage$Tage, Vorwarnzeit_effektiv=Vorwarnzeit-21)
@@ -276,15 +320,37 @@ for (h in 0:horizont) {
     filter(row_number()<=8) %>% # genau wie oben, 8 statt 7!
     summarise(Faelle_letzte_7_Tage=first(cases)-last(cases)) %>%
     mutate(Faelle_letzte_7_Tage_pro_Tag=round(Faelle_letzte_7_Tage/7))
+  letzte_7_tage_altersgruppen_destatis_h <- rki_alter_destatis %>%
+    mutate(date=date(Meldedatum)) %>%
+    filter(date<=stichtag) %>%
+    group_by(id) %>% arrange(id,-as.numeric(date)) %>%
+    filter(row_number()<=7) %>% # hier sind tatsächliche Fälle und nicht cumsum
+    summarise(`Faelle_letzte_7_Tage_0-14`=sum(`Fälle_0-15`),
+              `Faelle_letzte_7_Tage_15-34`=sum(`Fälle_15-34`),
+              `Faelle_letzte_7_Tage_35-59`=sum(`Fälle_35-59`),
+              `Faelle_letzte_7_Tage_60+`=sum(`Fälle_60+`)) %>%
+    ungroup() %>%
+    left_join(., strukturdaten, by="id") %>%
+    mutate(`Faelle_letzte_7_Tage_je100TsdEinw_0-14`=round(`Faelle_letzte_7_Tage_0-14`/((`unter 3 Jahre`+`3 bis unter 6 Jahre`+`6 bis unter 10 Jahre`+`10 bis unter 15 Jahre`)/100000)),
+           `Faelle_letzte_7_Tage_je100TsdEinw_15-34`=round(`Faelle_letzte_7_Tage_15-34`/((`15 bis unter 18 Jahre`+`18 bis unter 20 Jahre`+`20 bis unter 25 Jahre`+`25 bis unter 30 Jahre`+`30 bis unter 35 Jahre`)/100000)),
+           `Faelle_letzte_7_Tage_je100TsdEinw_35-59`=round(`Faelle_letzte_7_Tage_35-59`/((`35 bis unter 40 Jahre`+`40 bis unter 45 Jahre`+`45 bis unter 50 Jahre`+`50 bis unter 55 Jahre`+`55 bis unter 60 Jahre`)/100000)),
+           `Faelle_letzte_7_Tage_je100TsdEinw_60+`=round(`Faelle_letzte_7_Tage_60+`/((`60 bis unter 65 Jahre`+`65 bis unter 75 Jahre`+`75 Jahre und mehr`)/100000)))
+  
   ausgangsdaten_h <- aktuell  %>%
-    select(id,name,ICU_Betten,Einwohner,ebene,
+    select(id,name,ICU_Betten,EW_insgesamt,ebene,
            cases,R0) %>% filter(ebene!="Staaten" & !is.na(ebene)) %>% select(-ebene) %>% collect() %>%
     left_join(.,letzte_7_tage_h,by="id") %>%
-    mutate(Faelle_letzte_7_Tage_je100TsdEinw=round(Faelle_letzte_7_Tage/(Einwohner/100000)),
+    left_join(., letzte_7_tage_altersgruppen_destatis_h %>% select(
+      id,
+      `Faelle_letzte_7_Tage_je100TsdEinw_0-14`,
+      `Faelle_letzte_7_Tage_je100TsdEinw_15-34`,
+      `Faelle_letzte_7_Tage_je100TsdEinw_35-59`,
+      `Faelle_letzte_7_Tage_je100TsdEinw_60+`), by="id") %>%
+    mutate(Faelle_letzte_7_Tage_je100TsdEinw=round(Faelle_letzte_7_Tage/(EW_insgesamt/100000)),
            Faelle_letzte_7_Tage_je100TsdEinw=ifelse(Faelle_letzte_7_Tage_je100TsdEinw<0,NA,Faelle_letzte_7_Tage_je100TsdEinw))
 
   vorwarnzeitergebnis_h <- ausgangsdaten_h %>%
-    mutate(Handlungsgrenze_7_tage=50*(Einwohner/100000),
+    mutate(Handlungsgrenze_7_tage=50*(EW_insgesamt/100000),
            Handlungsgrenze_pro_Tag=round(Handlungsgrenze_7_tage/7),
            R0 = ifelse((R0>1) & (Faelle_letzte_7_Tage_pro_Tag==0),NA,R0),
            Kapazitaet=ICU_Betten*0.25/share_icu/icu_days,
@@ -292,7 +358,7 @@ for (h in 0:horizont) {
     filter(id<=16)
 
   myTage <- vorwarnzeitergebnis_h %>% rowwise() %>%
-    do(Tage = vorwarnzeit_berechnen(.$Einwohner, .$cases,.$Faelle_letzte_7_Tage_pro_Tag,.$Kapazitaet,1.3)) %>%
+    do(Tage = vorwarnzeit_berechnen(.$EW_insgesamt, .$cases,.$Faelle_letzte_7_Tage_pro_Tag,.$Kapazitaet,1.3)) %>%
     unnest(cols = c(Tage), keep_empty=TRUE)
   vorwarnzeitergebnis_h <- vorwarnzeitergebnis_h %>%
     mutate(Vorwarnzeit = myTage$Tage, Vorwarnzeit_effektiv=Vorwarnzeit-21, date=stichtag)
@@ -363,7 +429,7 @@ myplot <- ggplot(rki_divi_n_alter %>%
                           "Todesfälle an Fällen"= `Todesfälle`) %>% 
                    gather(Merkmal,Anteil,2:4) %>% mutate(Anteil=round(Anteil*100,digits=2)) ,
                  aes(x=Meldedatum,y=Anteil,color=Merkmal)) + geom_line() + theme_minimal() + 
-  scale_color_zi() + labs(y="Anteil in %",x="Datum",color="") + 
+  scale_color_zi() + labs(y="Verhältnis in %",x="Datum",color="") + 
   scale_x_date(breaks="1 month", date_labels = "%d.%m.") + 
   theme(panel.grid.major.x =   element_blank(),panel.grid.minor.x =   element_blank())
 myplot %>% ggplotly(tooltip = c("x", "y")) %>% 
@@ -458,8 +524,8 @@ Reaktionszeit <- 21
 Belastungsgrenze <- fall$Kapazitaet
 gamma=1/10
 for (i in seq(Rt)) {
-  mysir <- sirmodel(ngesamt = fall$Einwohner,
-                    S = fall$Einwohner - fall$cases,
+  mysir <- sirmodel(ngesamt = fall$EW_insgesamt,
+                    S = fall$EW_insgesamt - fall$cases,
                     I = (fall$Faelle_letzte_7_Tage_pro_Tag)/gamma,
                     R = fall$cases - (fall$Faelle_letzte_7_Tage_pro_Tag)/gamma,
                     R0 = Rt[i],
@@ -473,30 +539,39 @@ as_tibble(cbind(Rt,Vorwarnzeit)) %>%
 }
 
 rki_fallzahl_bl <- function(){
-  df <- vorwarnzeitergebnis %>% filter(id<17) %>% mutate(cases_je_100Tsd=round(cases/(Einwohner/100000)),
+  df <- vorwarnzeitergebnis %>% filter(id<17) %>% mutate(cases_je_100Tsd=round(cases/(EW_insgesamt/100000)),
                                                          R0=round(R0,digits = 2))
   df %>% select(Bundesland=name,
-                "Neue Fälle pro Tag"=Faelle_letzte_7_Tage_pro_Tag,
-                "Neue Fälle je 100 Tsd. Einw. in 7 Tagen"=Faelle_letzte_7_Tage_je100TsdEinw,
+                "R(t)"=R0,
+                "7-Tage-Inzidenz"=Faelle_letzte_7_Tage_je100TsdEinw,
+                "Effektive Vorwarnzeit aktuell"=Vorwarnzeit_effektiv,
+                "7-Tage-Inzidenz 60+"=`Faelle_letzte_7_Tage_je100TsdEinw_60+`,
+                "7-Tage-Inzidenz 35-59"=`Faelle_letzte_7_Tage_je100TsdEinw_35-59`,
+                "7-Tage-Inzidenz 15-34"=`Faelle_letzte_7_Tage_je100TsdEinw_15-34`,
+                "7-Tage-Inzidenz 0-14"=`Faelle_letzte_7_Tage_je100TsdEinw_0-14`,
                 "Fälle insgesamt"=cases,
                 "Fälle je 100 Tsd. Einw."=cases_je_100Tsd,
-                "R(t)"=R0,
-                "Effektive Vorwarnzeit aktuell"=Vorwarnzeit_effektiv)
+                "Neue Fälle pro Tag"=Faelle_letzte_7_Tage_pro_Tag
+  )
 }
 
 rki_fallzahl_kreis <- function(){
-  df <- vorwarnzeitergebnis %>% filter(id>17 | name=="Berlin") %>% mutate(cases_je_100Tsd=round(cases/(Einwohner/100000)),
+  df <- vorwarnzeitergebnis %>% filter(id>17 | name=="Berlin") %>% mutate(cases_je_100Tsd=round(cases/(EW_insgesamt/100000)),
                                                          R0=round(R0,digits = 2))
   bundeslaender <- aktuell %>% filter(id>0 & id<17) %>% select(blid=id,Bundesland=name) %>% collect()
   df <- df %>% mutate(blid=ifelse(id>17,floor(id/1000000),id)) %>% left_join(.,bundeslaender) %>% arrange(blid,id)
   df %>% select(Kreis=name,
                 Bundesland,
-                "Neue Fälle pro Tag"=Faelle_letzte_7_Tage_pro_Tag,
-                "Neue Fälle je 100 Tsd. Einw. in 7 Tagen"=Faelle_letzte_7_Tage_je100TsdEinw,
+                "R(t)"=R0,
+                "7-Tage-Inzidenz"=Faelle_letzte_7_Tage_je100TsdEinw,
+                "Effektive Vorwarnzeit lokal*"=Vorwarnzeit_effektiv, # needs communication
+                "7-Tage-Inzidenz 60+"=`Faelle_letzte_7_Tage_je100TsdEinw_60+`,
+                "7-Tage-Inzidenz 35-59"=`Faelle_letzte_7_Tage_je100TsdEinw_35-59`,
+                "7-Tage-Inzidenz 15-34"=`Faelle_letzte_7_Tage_je100TsdEinw_15-34`,
+                "7-Tage-Inzidenz 0-14"=`Faelle_letzte_7_Tage_je100TsdEinw_0-14`,
                 "Fälle insgesamt"=cases,
                 "Fälle je 100 Tsd. Einw."=cases_je_100Tsd,
-                "R(t)"=R0,
-                "Effektive Vorwarnzeit lokal*"=Vorwarnzeit_effektiv # needs communication
+                "Neue Fälle pro Tag"=Faelle_letzte_7_Tage_pro_Tag
                 )
 }
 
