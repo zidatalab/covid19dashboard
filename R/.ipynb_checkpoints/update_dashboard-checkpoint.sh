09c18f97c8826bcafcbde78ebce3fd402c7c1fae
Edgar Steiger <esteiger@zi.de>
2020-11-18 14:40:45 +0100

    fail on zimain

--- R/.ipynb_checkpoints/update_dashboard-checkpoint.sh
+++ R/.ipynb_checkpoints/update_dashboard-checkpoint.sh
@@ -2,14 +2,14 @@
 echo "Read Env Variables"
 source ~/zi_covid/myenvs.sh
 echo "Starting conda"
-source /opt/tljh/user/bin/conda
-conda activate "edgar_covid_r"
+# source /opt/tljh/user/bin/conda
+conda activate "edgar_covid_r4"
 echo "Pulling Code from git"
 cd ~/zi_covid/covid19dashboard/
 git pull
 echo "Starting DB-Update"
 cd ~/zi_covid/covid19dashboard/R/
-/home/jupyter-esteiger/.conda/envs/edgar_covid_r/bin/R CMD BATCH renderdashboard.R
+/home/jupyter-esteiger/.conda/envs/edgar_covid_r4/bin/R CMD BATCH renderdashboard.R
 cd ~/zi_covid/covid19dashboard/
 git add *
 git commit -m "Auto update Dashboard"
