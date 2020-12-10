d3e0b78fbaa1a8a911e0a8044dbeec358174830a
Edgar <edgar.steiger+github@gmail.com>
2020-12-10 17:50:02 +0100

    #37

--- R/legacy_code/rki_archive.R
+++ R/legacy_code/rki_archive.R
@@ -16,7 +16,18 @@ conn <- DBI::dbConnect(RPostgres::Postgres(),
 startrki <- tbl(conn,paste0("rki_", as_date(startdate))) %>% collect()
 secondrki <- tbl(conn,paste0("rki_", as_date(startdate)+1)) %>% collect()
 
-endrki <- tbl(conn, paste0("rki_", as_date(enddate))) %>% collect()
-dbWriteTable(conn, "rki", endrki, overwrite=TRUE)
+dbWriteTable(conn, "rki_archive", startrki, overwrite=TRUE)
+dbWriteTable(conn, "rki_archive", secondrki, append=TRUE)
+
+for (idx in seq(2, 15)) {
+  thisrki <- tbl(conn,paste0("rki_", as_date(startdate)+idx)) %>% collect()
+  tt <- system.time({
+    dbWriteTable(conn, "rki_archive", thisrki, append=TRUE)
+  })
+  cat(as_date(startdate)+idx, ": ", tt[3]/60, "minutes\n")
+}
 
 testarchive <- tbl(conn, "rki_archive") %>% collect()
+tt <- system.time({testarchive_teil <- tbl(conn, "rki_archive") %>% 
+  filter(Datenstand=="2020-12-06") %>% 
+  collect() })
