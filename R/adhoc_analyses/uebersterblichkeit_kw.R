4dff2d10ea5e4a573e46a722847b757cb1e77d37
Edgar <edgar.steiger+github@gmail.com>
2020-12-14 13:21:43 +0100

    slight aesthethic fixes to code

--- R/adhoc_analyses/uebersterblichkeit_kw.R
+++ R/adhoc_analyses/uebersterblichkeit_kw.R
@@ -11,10 +11,12 @@ url <- "https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Sterb
 destfile <- "~/Downloads/sonderauswertung_sterbefaelle.xlsx"
 curl::curl_download(url, destfile)
 sterbefaelle_tage <- read_excel(destfile, 
-                                sheet = "D_2016_2020_Tage", skip = 8) %>%
-  rename(Jahr="...1") %>% gather(Tag,Faelle,2:ncol(.))
-
-
+                                sheet = "D_2016_2020_Tage",
+                                skip = 8,
+                                na="X") %>%
+  rename(Jahr="...1") %>% 
+  pivot_longer(cols=-Jahr, names_to="Tag", values_to="Faelle")
+  # gather(Tag,Faelle,2:ncol(.))
 
 sterbefaelle_kw <- bind_rows(read_excel(destfile, 
                               sheet = "D_2016_2020_KW_AG_Männlich", 
@@ -22,50 +24,84 @@ sterbefaelle_kw <- bind_rows(read_excel(destfile,
                              read_excel(destfile, 
                                         sheet = "D_2016_2020_KW_AG_Weiblich", 
                                         skip = 8) %>% mutate(sex="weiblich")) %>%
-  select(-"Nr.") %>% rename("Jahr"="...2", "Alter"= "unter … Jahren" ) %>% relocate(Jahr,Alter,sex) %>% gather(KW,Tote,4:ncol(.))
+  select(-"Nr.") %>% 
+  rename("Jahr"="...2", "Alter"= "unter … Jahren" ) %>%
+  relocate(Jahr,Alter,sex) %>% 
+  pivot_longer(cols=-c("Jahr", "Alter", "sex"), names_to="KW", values_to="Tote")
+  # gather(KW,Tote,4:ncol(.))
 
 
 
 sterbefaelle_kw.rec <- 
-  left_join(sterbefaelle_kw %>% filter(Jahr==2020 & !is.na(Tote)) ,
-  sterbefaelle_kw %>% filter(Jahr<2020) %>% group_by(Alter,KW,sex) %>% summarise(Tote_2016_2019=mean(Tote,na.rm=T)),
-  by=c("KW","Alter","sex")) %>% ungroup() %>%
+  left_join(sterbefaelle_kw %>% filter(Jahr==2020 & !is.na(Tote)),
+            sterbefaelle_kw %>% filter(Jahr<2020) %>%
+              group_by(Alter,KW,sex) %>% 
+              summarise(Tote_2016_2019=mean(Tote,na.rm=T), .groups="drop"),
+            by=c("KW","Alter","sex")) %>% 
   mutate(
     KW=as.numeric(KW),
     Vergleich=(Tote/Tote_2016_2019)*100,
-         startage=as.numeric(stringr::str_split_fixed(Alter,"-",2)[,1]),
-         stopage=as.numeric(stringr::str_split_fixed(Alter,"-",2)[,2]),
-         agegrp = case_when(stopage<=60 ~ 1,
-                            startage>=60 &  startage<=75~ 2,
-                            startage>=80 ~ 3,
-                            Alter=="95 u. mehr" ~ 3,
-                            Alter=="Insgesamt" ~ 4
-                            ),
-        agegrp=factor(agegrp,ordered = T,levels=c(1,2,3,4),labels=c("0-59","60-79","80+","Gesamt"))
-         ) 
+    startage=as.numeric(ifelse(grepl("-", Alter), stringr::str_split_fixed(Alter,"-",2)[,1], NA)),
+    stopage=as.numeric(ifelse(grepl("-", Alter), stringr::str_split_fixed(Alter,"-",2)[,2], NA)),
+    agegrp = case_when(
+      stopage<=60 ~ 1,
+      startage>=60 & startage<=75 ~ 2,
+      startage>=80 ~ 3,
+      Alter=="95 u. mehr" ~ 3,
+      Alter=="Insgesamt" ~ 4
+      ),
+    agegrp=factor(agegrp,
+                  ordered = T,
+                  levels=c(1,2,3,4),
+                  labels=c("0-59","60-79","80+","Gesamt"))
+    ) 
 
 
 min_kw <- min(sterbefaelle_kw.rec$KW)
 max_kw <- max(sterbefaelle_kw.rec$KW)
 
-myplot <- sterbefaelle_kw.rec %>% group_by(agegrp,KW) %>% filter(agegrp!="Gesamt") %>% summarise(Vergleich=100*(sum(Tote)/sum(Tote_2016_2019))) %>% filter(!is.na(agegrp)) %>% 
+myplot <- sterbefaelle_kw.rec %>% 
+  group_by(agegrp,KW) %>% 
+  filter(agegrp!="Gesamt") %>% 
+  summarise(Vergleich=100*(sum(Tote)/sum(Tote_2016_2019)),
+            .groups="drop") %>% 
+  filter(!is.na(agegrp)) %>% 
   ggplot(aes(x=KW,y=Vergleich,group=agegrp,color=agegrp)) + 
-  geom_line(show.legend = F, size=1.5) +  scale_color_zi() +
-  theme_zi_titels() + geom_label(data = . %>% 
-                                   ungroup() %>% group_by(agegrp) %>% arrange(-KW) %>% filter(KW>40) %>% filter(row_number()==1),aes(label=agegrp),show.legend = F) + geom_hline(yintercept = 100) + 
+  geom_line(show.legend = F, size=1.5) + 
+  scale_color_zi() +
+  theme_zi_titels() +
+  geom_label(data = . %>% 
+               group_by(agegrp) %>%
+               arrange(-KW) %>% 
+               filter(KW>40) %>%
+               filter(row_number()==1),
+             aes(label=agegrp),
+             show.legend = F) + 
+  geom_hline(yintercept = 100) + 
   labs(y="Todesfälle in % von 2016-19", 
        x="Kalenderwoche")
 
 
-myplot_1_alt <- sterbefaelle_kw.rec %>% group_by(agegrp,KW) %>% filter(agegrp!="Gesamt") %>% summarise(Vergleich=100*(sum(Tote)/sum(Tote_2016_2019))) %>% filter(!is.na(agegrp)) %>%   group_by(agegrp) %>% arrange(KW) %>%
+myplot_1_alt <- sterbefaelle_kw.rec %>% 
+  group_by(agegrp,KW) %>%
+  filter(agegrp!="Gesamt") %>%
+  summarise(Vergleich=100*(sum(Tote)/sum(Tote_2016_2019)),
+            .groups="drop") %>%
+  filter(!is.na(agegrp)) %>%  
+  group_by(agegrp) %>% 
+  arrange(KW) %>%
   mutate(Datum=as_date("2020-01-01")+weeks(as.numeric(KW)),
          Vergleich=rollmean(Vergleich, k = 3, fill = NA)) %>%
   ggplot(aes(x=Datum,y=Vergleich,group=agegrp,color=agegrp)) + 
-  geom_line(show.legend = F, size=1.5) +  scale_color_zi() +
+  geom_line(show.legend = F, size=1.5) + 
+  scale_color_zi() +
   theme_zi_titels() + 
-  geom_label(data=. %>% filter(!is.na(Vergleich))%>% 
-              group_by(agegrp) %>% arrange(-as.numeric(Datum)) %>% filter(row_number()==1) ,
-              aes(label=agegrp),show.legend = FALSE)+
+  geom_label(data=. %>% 
+               filter(!is.na(Vergleich)) %>% 
+               group_by(agegrp) %>%
+               arrange(-as.numeric(Datum)) %>% 
+               filter(row_number()==1),
+             aes(label=agegrp),show.legend = FALSE) +
   geom_hline(yintercept = 100) + 
   labs(y="Todesfälle in % von 2016-19", 
        x="Datum") + scale_x_date(breaks="4 weeks",date_labels = "%d.%m.")
