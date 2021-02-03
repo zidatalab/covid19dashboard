f0a27549997d5beed889ece9b5924333f086cb23
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-02-03 08:27:33 +0100

    Start Impfmodellierung

---
+++ R/adhoc_analyses/Impfmodellierung.R
@@ -0,0 +1,54 @@
+
+library("tidyverse")
+library("lubridate")
+
+# Impfmodellierung
+
+# das Corona-Kabinett hat Modellierungen erbeten, wie sich die Zahl der zu 
+# verimpfenden Dosen, die verfügbare Impfkapazität in den Zentren 
+#  (Annahmen u.a. unter Normalbetreib / verlängerte Öffnungszeiten / 
+# unter 24/7 Betrieb etc) und Zeitpunkt des Wechsels in die vertragsärztliche 
+# Versorgung entwickeln könnte. Dabei ist ja auch zu berücksichtigen, welcher 
+# Impfstoff sich überhaupt eignet für die vertragsärztliche Versorgung.
+
+# In der Anlage finden Sie unter III. die erwarteten Zahlen bezogen auf die 
+# Quartale. Könnten Sie für Q2 und Q3 Modellierungen vornehmen lassen, auch
+# da mit verschiedenen Varianten (zB Variante 1 bezogen auf BioNTech:
+# Quartalsdosenzahl verteilt sich gleichmäßig auf die drei Monate 
+# (also 31,5 Mio. Dosen BioNtech : 3 Monate = 10,5 Mio. Dosen / Monat); 
+# Variante 2: 20 % der 31,5 Mio. Dosen in April, weitere 35 % in Mai und 45 % 
+# in Juni (oder so)), wie sich Kapazitäten entwickeln müssen und wie sich die
+# Phasen von Impfzentrum in Praxen entwickeln könnten?
+  
+
+# Outcomes
+
+## 1. Zahl der zu verimpfenden Dosen
+## 2. Verfügbare Impfkapazität in den Zentren
+## 3. Zeitpunkt des Wechsels in die vertragsärztliche  Versorgung
+
+## Modellierung
+
+## Zeit: 
+### Q2 und Q3 nach KW
+
+## Parameter:
+## Betriebszeit: Normalbetreib / verlängerte Öffnungszeiten / 24/7 Betrieb 
+## Verfügbarkeit: V1 Biontech, V2 
+
+## Datenbasis
+impfstart = as.Date("2020-12-20")
+prognoseende = as.Date("2021-12-31")
+
+dosen = tibble(dosen=c(650*1e3, 9.15*1e6, 43.6*1e6,74.3*1e6,52.4*1e6),
+                   quartal=c(4,1,2,3,4),
+                   jahr=c(2020,2021,2021,2021,2021))
+
+prognosedatensatz <- tibble(Datum=impfstart+days(seq(0,as.integer(prognoseende-impfstart),1))) %>%
+  mutate(kw=isoweek(Datum),
+         jahr=year(Datum),
+         quartal=quarter(Datum))
+
+
+
+
