efe2d99e67a64924ad4329fc142515d10ca2584f
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-02-03 12:12:29 +0100

    acknowledge hersteller

--- R/adhoc_analyses/Impfmodellierung.R
+++ R/adhoc_analyses/Impfmodellierung.R
@@ -279,10 +279,29 @@ library("lubridate")
 impfstart = as.Date("2020-12-20")
 prognoseende = as.Date("2021-12-31")
 
-dosen = tibble(dosen=c(650*1e3, 9.15*1e6, 43.6*1e6,74.3*1e6,52.4*1e6),
+dosen = tibble(dosen_bt=c(1.3*1e6, 10.9*1e6, (31.5+8.7)*1e6,(17.6+17.1)*1e6,(2.7+10.8)*1e6),
+               dosen_mod=c(0, 1.8*1e6, 6.4*1e6,(17.6+9.1)*1e6,(24.6+18.3)*1e6),
+               dosen_az=c(0, 5.6*1e6, 16.9*1e6,33.8*1e6,0*1e6),
+               dosen_jj=c(0, 0, 10.1*1e6,22*1e6,4.6*1e6),
+               dosen_cv=c(0, 0, 3.5*1e6,9.4*1e6,11.7*1e6),
+               dosen_gsk=c(0, 0, 0,0,27.5*1e6),
                    quartal=c(4,1,2,3,4),
-                   jahr=c(2020,2021,2021,2021,2021))
+                   jahr=c(2020,2021,2021,2021,2021)) %>%
+  gather(hersteller,dosen,contains("dosen")) %>% mutate(hersteller=str_replace(hersteller,"dosen_","")) %>% mutate(anwendungen=ifelse(hersteller!="jj",2,1),abstand=case_when(hersteller=="bt"~4*7,hersteller=="mod"~6*7,hersteller=="jj"~0,TRUE~3*7))
 
+
+
+# CHECKs 
+# dosen %>% group_by(hersteller) %>% 
+#   summarise(dosen=sum(dosen),
+#             anwendungen=sum(dosen)/mean(anwendungen),
+#             abstand=mean(abstand)) 
+# 
+# dosen %>% group_by(hersteller) %>% 
+#   summarise(dosen=sum(dosen),
+#             anwendungen=sum(dosen)/mean(anwendungen),
+#             abstand=mean(abstand)) %>% ungroup() %>% summarise(anwendungen=sum(anwendungen)) 
+  
 # Kapazitäten
 n_impfzentren <- 400
 impfzentrum_kapazitaet_wt <- 200e3/n_impfzentren
@@ -291,30 +310,34 @@ n_praxen <- 50000
 praxis_kapoaziteat_wt = 10
 
 
-prognosedatensatz <- tibble(Datum=impfstart+days(seq(0,as.integer(prognoseende-impfstart),1))) %>%
-  mutate(kw=isoweek(Datum),
-         jahr=year(Datum),
-         quartal=quarter(Datum),
-         werktag=ifelse(weekdays(Datum) %in%c("Samstag","Sonntag"),0,1 ))
 
 # Input-Daten
-andata <- left_join(prognosedatensatz , dosen, by=c("jahr","quartal")) %>%
-  group_by(jahr,quartal) %>%
-  mutate(dosen.verf= dosen/n()) %>% ungroup() 
+andata <- full_join(prognosedatensatz , dosen, by=c("jahr","quartal")) %>%
+  group_by(hersteller,jahr,quartal) %>%
+  mutate(dosen.verf= dosen/n()) %>% ungroup() %>% arrange(hersteller,as.numeric(Datum))
+
+# check 
+# sum(andata$dosen.verf/andata$anwendungen)/1e6
+
 
 # Output-Daten
-output <- andata %>% 
-  mutate(iz_regelbetrieb=n_impfzentren*impfzentrum_kapazitaet_wt*werktag,
-         iz_7tw=n_impfzentren*impfzentrum_kapazitaet_wt) %>% 
-  gather(Betriebsart,Kapazitaet,contains("iz_")) %>%
-  group_by(Betriebsart) %>%
-  mutate(iz_Restdosen=ifelse(cumsum(Kapazitaet)>=cumsum(dosen.verf),0,
-                             cumsum(dosen.verf)-cumsum(Kapazitaet)),
-         iz_Verimpft = cumsum(dosen.verf)- iz_Restdosen,
-         praxen_kapazitaet=n_praxen*praxis_kapoaziteat_wt*werktag,
-         praxen_Restdosen=ifelse(praxen_kapazitaet>=cumsum(iz_Restdosen),0,
-                                 cumsum(cumsum(iz_Restdosen)-praxen_kapazitaet)) )
+# BROKEN
+# output <- andata %>% 
+#   mutate(iz_regelbetrieb=n_impfzentren*impfzentrum_kapazitaet_wt*werktag,
+#          iz_7tw=n_impfzentren*impfzentrum_kapazitaet_wt) %>% 
+#   gather(Betriebsart,Kapazitaet,contains("iz_")) %>%
+#   group_by(Betriebsart) %>%
+#   mutate(iz_Restdosen=ifelse(cumsum(Kapazitaet)>=cumsum(dosen.verf),0,
+#                              cumsum(dosen.verf)-cumsum(Kapazitaet)),
+#          iz_Verimpft = cumsum(dosen.verf)- iz_Restdosen,
+#          praxen_kapazitaet=n_praxen*praxis_kapoaziteat_wt*werktag,
+#          praxen_Restdosen=ifelse(praxen_kapazitaet>=cumsum(iz_Restdosen),0,
+#                                  cumsum(cumsum(iz_Restdosen)-praxen_kapazitaet)) )
+
+# DEBUG PLOTS
+ggplot(andata) + aes(x=Datum,y=cumsum(dosen.verf)/1e6) + geom_line() + scale_y_continuous(limits=c(0,sum(andata$dosen.verf)/1e6))
+ggplot(output) + aes(x=Datum,y=iz_Restdosen/1e6,color=Betriebsart) + geom_line() + scale_y_continuous(limits=c(0,sum(andata$dosen.verf)/1e6))
+ggplot(output) + aes(x=Datum,y=praxen_Restdosen/1e6,color=Betriebsart) + geom_line() + scale_y_continuous(limits=c(0,sum(andata$dosen.verf)/1e6))
 
 
-  
                           
