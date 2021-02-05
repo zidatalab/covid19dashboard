efffb35247a5694e5e8a3453eee8495e88d58d38
Edgar <edgar.steiger+github@gmail.com>
2021-02-05 14:37:48 +0100

    check and update dahsobard only if new data

--- R/renderdashboard.R
+++ R/renderdashboard.R
@@ -1,1 +1,17 @@
-rmarkdown::render('../Start.Rmd')
+library(lubridate)
+library(dplyr)
+conn <- DBI::dbConnect(RPostgres::Postgres(),
+                       host   = Sys.getenv("DBHOST"),
+                       dbname = Sys.getenv("DBNAME"),
+                       user      =  Sys.getenv("DBUSER"),
+                       password        = Sys.getenv("DBPASSWORD"),
+                       port     = 5432,
+                       sslmode = 'require')
+
+RepoStand <- as_date((tbl(conn,"Stand") %>% collect())$Datum)
+
+if (RepoStand<today()) {
+  rmarkdown::render('../Start.Rmd')
+}
+
+DBI::dbDisconnect(conn)
