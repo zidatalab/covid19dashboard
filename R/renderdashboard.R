7974ba8b75fb1a8544574dc3931555f42d407574
Edgar <edgar.steiger+github@gmail.com>
2021-02-06 09:20:03 +0100

    fix check for dashboard update

--- R/renderdashboard.R
+++ R/renderdashboard.R
@@ -1,5 +1,6 @@
 library(lubridate)
 library(dplyr)
+library(stringr)
 conn <- DBI::dbConnect(RPostgres::Postgres(),
                        host   = Sys.getenv("DBHOST"),
                        dbname = Sys.getenv("DBNAME"),
@@ -9,9 +10,16 @@ conn <- DBI::dbConnect(RPostgres::Postgres(),
                        sslmode = 'require')
 
 RepoStand <- as_date((tbl(conn,"Stand") %>% collect())$Datum)
+DashboardStand <- as_date((tbl(conn,"Dashboardstand") %>% collect())$Datum)
 
-if (RepoStand<today()) {
+if (Dashboardstand<RepoStand) {
   rmarkdown::render('../Start.Rmd')
+  Dashboardstand <- tibble("Datum"=as.character(date(now())),
+                         "Zeit"=paste0(str_pad(format(hour(now())),2, pad = "0"),
+                                       ":",
+                                       str_pad(format(minute(now())),2, pad = "0"),
+                                       " Uhr"))
+  DBI::dbWriteTable(conn,"Dashboardstand",Dashboardstand,overwrite=TRUE)
 }
 
 DBI::dbDisconnect(conn)
