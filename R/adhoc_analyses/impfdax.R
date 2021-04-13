82610b9f546e1cef2abe581f3916a0ceb1f98a7a
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-04-13 10:17:28 +0200

    new data for impfdax

--- R/adhoc_analyses/impfdax.R
+++ R/adhoc_analyses/impfdax.R
@@ -102,7 +102,7 @@ export_bl_impfungen <- impfungen.raw.bl %>%
 write_csv(export_bl_impfungen ,"data/tabledata/impfdax_bl_stand.csv")
 
 praxen_wirkstoffe_aktuell <- impfungen_praxen_bl.raw %>% filter(kw>=(isoweek(now())-1)) %>% group_by(Bundesland,vacc_product) %>% summarise(Impfungen=sum(anzahl,na.rm = TRUE)) %>%  spread(vacc_product,Impfungen,fill = 0)
-write_csv(export_bl_impfungen ,"data/tabledata/impfdax_praxen_wirkstoffe_aktuell.csv")
+write_csv(praxen_wirkstoffe_aktuell ,"data/tabledata/impfdax_praxen_wirkstoffe_aktuell.csv")
 
 
 # Datenstand
