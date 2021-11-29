f7fdaac348325982aa16afe33065eb973c1291ea
Edgar Steiger <edgar.steiger@gmail.com>
2021-11-29 11:02:16 +0100

    gdm november 2021

--- R/adhoc_analyses/grafik_7tageschnitt_erstimpfungen_praxen.R
+++ R/adhoc_analyses/grafik_7tageschnitt_erstimpfungen_praxen.R
@@ -28,14 +28,14 @@ stschnitt <- impfungen_tc %>%
   select(Datum, `Erst-Praxen`) %>% 
   mutate(Datum=as_date(Datum, origin = as_date("1899-12-30"))) %>% 
   mutate(stschnitt=rollmean(`Erst-Praxen`, 7, fill = 0, align = "left")) %>% 
-  filter(Datum>="2021-11-01") %>% 
+  filter(Datum>="2021-11-15") %>% 
   select(Datum, stschnitt)
 
 stschnitt_plot <- ggplot(stschnitt,
-                         aes(x=Datum, y=stschnitt)) + #, label=round(stschnitt)
-  # geom_bar(stat="identity", fill=zi_cols("ziblue")) +
-  # coord_flip()+
-  geom_line(col=zi_cols("ziblue"), size=3) +
+                         aes(x=Datum, y=stschnitt, label=round(stschnitt))) + #
+  geom_bar(stat="identity", fill=zi_cols("ziblue")) +
+  coord_flip()+
+  # geom_line(col=zi_cols("ziblue"), size=3) +
   scale_y_continuous(labels=scales::comma_format(decimal.mark = ",", 
                                                  big.mark = "."),
                      limits=c(0, NA)) +
@@ -43,11 +43,11 @@ stschnitt_plot <- ggplot(stschnitt,
   theme(axis.text.x = element_text(angle=45)) +
   labs(title = "Anstieg der Erstimpfungen in ärztlichen Praxen",
        subtitle = "Sieben-Tages-Durchschnitt der Erstimpfungen (Nov. 2021)") +
-  # geom_text(size = 3, hjust=1.1, col="white") +
+  geom_text(size = 3, hjust=1.1, col="white") +
   scale_x_date(#date_breaks="days", 
                breaks=as_date(min(stschnitt$Datum):max(stschnitt$Datum)),
                date_labels = "%d.%m.")  # , limits = as_date(c(min(stschnitt$Datum), max(stschnitt$Datum)))
-  
+stschnitt_plot  
 
 finalise_plot(stschnitt_plot,
               save_filepath = "data/siebentageschnitterstimpfungenpraxen.png",
