3795c56db06d311b9449dca55d838b99939d0cd6
Edgar Steiger <edgar.steiger@gmail.com>
2021-12-20 15:16:10 +0100

    für bremen impfupdate

---
+++ R/adhoc_analyses/bremen_info_impfen.R
@@ -0,0 +1,90 @@
+library(lubridate)
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(readr)
+library(zoo)
+
+conn <- DBI::dbConnect(RPostgres::Postgres(),
+                       host   = Sys.getenv("DBHOST"),
+                       dbname = Sys.getenv("DBNAME"),
+                       user      =  Sys.getenv("DBUSER"),
+                       password        = Sys.getenv("DBPASSWORD"),
+                       port     = 5432,
+                       sslmode = 'require')
+
+kbv_rki_impfstoff <- tbl(conn, "kbv_rki_impfstoffe_laender") %>% 
+  collect()
+kbv_rki_age <- tbl(conn,"kbv_rki_altersgruppen_kreise") %>%
+  collect()
+
+kbv_rki_impfstoff_bremen <- kbv_rki_impfstoff %>% 
+  filter(Bundesland=="Bremen")
+kbv_rki_age_bremen <- kbv_rki_age %>% 
+  filter(kv=="Bremen")
+
+# bremen_gesamt <- kbv_rki_age_bremen %>% 
+#   summarise(gesamt=sum(anzahl_alleorte, na.rm = TRUE)) %>%
+#   pull(gesamt)
+# 
+# bremen_praxen_gesamt <- kbv_rki_age_bremen %>% 
+#   summarise(gesamt_praxen=sum(anzahl_praxen, na.rm = TRUE)) %>% 
+#   pull(gesamt_praxen)
+# 
+# anteil_praxen_bremen <- bremen_praxen_gesamt/bremen_gesamt
+
+bremen_gesamtezeit <- kbv_rki_age_bremen %>% 
+  group_by(vacc_series) %>% 
+  summarise(gesamtezeit_praxen=sum(anzahl_praxen, na.rm=TRUE),
+            gesamtezeit_gesamt=sum(anzahl_alleorte, na.rm=TRUE),
+            anteil_praxen=sum(gesamtezeit_praxen/gesamtezeit_gesamt)) %>% 
+  pivot_wider(names_from = vacc_series, values_from = gesamtezeit_praxen:anteil_praxen) %>% 
+  mutate('Land Bremen'="ingesamt") %>% 
+  select('Land Bremen',
+         'Erst-Praxen'=gesamtezeit_praxen_1,
+         'Erst-Gesamt'=gesamtezeit_gesamt_1,
+         'Erst-Anteil'=anteil_praxen_1,
+         'Zweit-Praxen'=gesamtezeit_praxen_2,
+         'Zweit-Gesamt'=gesamtezeit_gesamt_2,
+         'Zweit-Anteil'=anteil_praxen_2,
+         'Booster-Praxen'=gesamtezeit_praxen_3,
+         'Booster-Gesamt'=gesamtezeit_gesamt_3,
+         'Booster-Anteil'=anteil_praxen_3) %>% 
+  mutate('Alle-Praxen'=`Erst-Praxen`+`Zweit-Praxen`+`Booster-Praxen`,
+         'Alle-Gesamt'=`Erst-Gesamt`+`Zweit-Gesamt`+`Booster-Gesamt`,
+         "Alle-Anteil"=`Alle-Praxen`/`Alle-Gesamt`)
+
+bremen_letzte4wochen <- kbv_rki_age_bremen %>% 
+  filter(vacc_date>=today()-days(28)) %>% 
+  group_by(vacc_series) %>% 
+  summarise(letzte4wochen_praxen=sum(anzahl_praxen, na.rm=TRUE),
+            letzte4wochen_gesamt=sum(anzahl_alleorte, na.rm=TRUE),
+            anteil_praxen=sum(letzte4wochen_praxen/letzte4wochen_gesamt)) %>% 
+  pivot_wider(names_from = vacc_series, values_from = letzte4wochen_praxen:anteil_praxen) %>% 
+  mutate('Land Bremen'='letzte 4 Wochen') %>% 
+  select('Land Bremen',
+         'Erst-Praxen'=letzte4wochen_praxen_1,
+         'Erst-Gesamt'=letzte4wochen_gesamt_1,
+         'Erst-Anteil'=anteil_praxen_1,
+         'Zweit-Praxen'=letzte4wochen_praxen_2,
+         'Zweit-Gesamt'=letzte4wochen_gesamt_2,
+         'Zweit-Anteil'=anteil_praxen_2,
+         'Booster-Praxen'=letzte4wochen_praxen_3,
+         'Booster-Gesamt'=letzte4wochen_gesamt_3,
+         'Booster-Anteil'=anteil_praxen_3) %>% 
+  mutate('Alle-Praxen'=`Erst-Praxen`+`Zweit-Praxen`+`Booster-Praxen`,
+         'Alle-Gesamt'=`Erst-Gesamt`+`Zweit-Gesamt`+`Booster-Gesamt`,
+         "Alle-Anteil"=`Alle-Praxen`/`Alle-Gesamt`)
+
+tabelle_4wochen <- bind_rows(bremen_gesamtezeit,
+                             bremen_letzte4wochen)
+
+stschnitt <- kbv_rki_age_bremen %>% 
+  group_by(vacc_date) %>% 
+  summarise(impfungen_praxen=sum(anzahl_praxen, na.rm=TRUE),
+            impfungen_gesamt=sum(anzahl_alleorte, na.rm=TRUE)) %>% 
+  mutate(wtag=wday(vacc_date, week_start = 1)) %>% 
+  arrange(rev(vacc_date)) %>% 
+  mutate(stschnitt_praxen=rollmean(impfungen_praxen, 7, fill = 0, align = "left"),
+         stschnitt_gesamt=rollmean(impfungen_gesamt, 7, fill = 0, align = "left"))
+
