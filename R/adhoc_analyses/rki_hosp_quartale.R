3c65f398f402dec9ca7a2b52eb1384d753bdeae2
Edgar Steiger <edgar.steiger@gmail.com>
2022-01-25 14:44:58 +0100

    add bund

--- R/adhoc_analyses/rki_hosp_quartale.R
+++ R/adhoc_analyses/rki_hosp_quartale.R
@@ -7,3 +7,12 @@ nisa <- rki_hosp_bl %>%
   group_by(quartal, Jahr) %>% 
   summarise(hosp=sum(`7T_Hospitalisierung_Faelle`),
             .groups="drop")
+
+bund <- rki_hosp_bl %>% 
+  mutate(dow=wday(Datum, week_start = 1),
+         quartal=quarter(Datum),
+         Jahr=year(Datum)) %>% 
+  filter(dow==1) %>% 
+  group_by(quartal, Jahr) %>% 
+  summarise(hosp=sum(`7T_Hospitalisierung_Faelle`),
+            .groups="drop")
